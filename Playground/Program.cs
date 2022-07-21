4af15a855b7997d9a925147cf9a7cd2f14447a06
marcaldo <marcaldo@gmail.com>
2022-07-21 09:24:02 -0500

    Console playground project

---
+++ Playground/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+
+foreach (var culture in CultureInfo.GetCultures(CultureTypes.AllCultures))
+{
+    if (culture.Name.StartsWith("en"))
+    {
+        string decimalSeparator = culture.NumberFormat.NumberDecimalSeparator == "," ? "comma" : "dot";
+        Console.WriteLine($"{culture.NativeName} ({culture.Name}) {culture.DateTimeFormat.ShortDatePattern} {decimalSeparator}");
+    }
+}
